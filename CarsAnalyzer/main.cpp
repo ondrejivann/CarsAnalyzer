2b0414761cf53aa046ea771a0771c3361a4938ef
ondrejivan <xivan@mendelu.cz>
2020-10-28 14:14:20 +0100

    Initial Commit

---
+++ CarsAnalyzer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CarsAnalyzer
+//
+//  Created by Ondřej Ivan on 28/10/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
